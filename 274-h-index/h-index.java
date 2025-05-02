0f7e33a8fa2324d1f01471e6bdc304369ef29212
HARIPRASATH G <140241512+Hari-dev123@users.noreply.github.com>
2025-05-02 11:11:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (53.46%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,26 +1,26 @@
 class Solution {
     public int hIndex(int[] citations) {
-           sort(citations);
-           int idx = 0;
-           for(int i=0;i<citations.length;i++){
-                if(citations[i] >= i+1){
-                    idx++;
-                }else{
-                    break;
-                }
-           }
-           return idx;
+        int n = citations.length;
+          int [] newArr = new int[n + 1];
+          for(int c : citations){
+               if(c >= n){
+                newArr[n]++;
+               }else{
+                  newArr[c]++;
+               }
+
+          }
+          int total = 0;
+        n = newArr.length;
+          for(int i = n-1 ; i >=0;i--){
+               total += newArr[i];
+               if(total >= i){
+                  return i;
+               }
+          }
+          return 0;
 
     }
-    static void sort(int arr[]){
-        for(int i=0;i<arr.length-1;i++){
-              for(int j=i+1;j<arr.length;j++){
-                 if(arr[j] > arr[i]){
-                       int temp = arr[i];
-                       arr[i] = arr[j];
-                       arr[j] = temp;
-                 }
-              }
-        }
-    }
+    
+    
 }
