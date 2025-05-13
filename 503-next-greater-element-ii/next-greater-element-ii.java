ff15d8ff68cebe959325d171ee53f0877bf7fe07
HARIPRASATH G <140241512+Hari-dev123@users.noreply.github.com>
2025-05-13 17:02:35 +0530

    Time: 16 ms (59.04%) | Memory: 46.4 MB (50.23%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,29 +1,20 @@
 class Solution {
     public int[] nextGreaterElements(int[] arr) {
         int res[] = new int[arr.length];
-        for(int i=0;i<res.length;i++){
-            res[i] = -1;
-        }
-        for(int i=0;i<arr.length;i++){
-            boolean flag = true;
-             int num = arr[i];
-             for(int j=i+1;j<arr.length;j++){
-                  if(arr[j] > num){
-                      res[i] = arr[j];
-                      flag = false;
-                      break;
-                  }
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length;
+        for(int i=2*n-1;i>=0;i--){
+             int idx = i % n;
+             int num = arr[idx];
+             while(!st.isEmpty() && st.peek() <= num){
+                st.pop();
              }
-             if(flag){
-                 for(int k = 0; k< i;k++){
-                      if(arr[k] > num){
-                          res[i] = arr[k];
-                          break;
-                      }
-                 }
+
+             if(i < n ){
+                res[i] = st.isEmpty() ? -1 : st.peek();
              }
+             st.push(num);
         }
-        
-        return  res;
+        return res;
     }
 }
