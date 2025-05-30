380a10984b954d2dc210e17e35be3303dc2a911c
HARIPRASATH G <140241512+Hari-dev123@users.noreply.github.com>
2025-05-30 14:33:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (14.67%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,25 +1,38 @@
 public class Solution {
     public void gameOfLife(int[][] board) {
         int m = board.length, n = board[0].length;
-        int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
-        
+
+        int[][] directions = {{0,1},{1,0},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int live = 0;
-                for (int[] d : dirs) {
-                    int x = i + d[0], y = j + d[1];
-                    if (x >= 0 && x < m && y >= 0 && y < n && (board[x][y] & 1) == 1)
-                        live++;
+                int liveNeighbors = 0;
+
+                for (int[] dir : directions) {
+                    int ni = i + dir[0];
+                    int nj = j + dir[1];
+
+                    if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                        if (board[ni][nj] == 1 || board[ni][nj] == 2) {
+                            liveNeighbors++;
+                        }
+                    }
                 }
-                if ((board[i][j] & 1) == 1 && (live == 2 || live == 3))
-                    board[i][j] |= 2;
-                if ((board[i][j] & 1) == 0 && live == 3)
-                    board[i][j] |= 2;
+
+                if (board[i][j] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[i][j] = 2; 
+                }
+
+                if (board[i][j] == 0 && liveNeighbors == 3) {
+                    board[i][j] = 3; 
+                }
             }
         }
-        
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                board[i][j] >>= 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 2) board[i][j] = 0;
+                else if (board[i][j] == 3) board[i][j] = 1;
+            }
+        }
     }
 }
