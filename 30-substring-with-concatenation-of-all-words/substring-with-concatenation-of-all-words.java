1b8c12b1567244951ad8718ed879b88ed378449d
HARIPRASATH G <140241512+Hari-dev123@users.noreply.github.com>
2025-05-30 11:08:57 +0530

    Time: 13 ms (35.99%) | Memory: 45.6 MB (55.10%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,50 +1,50 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> ans = new ArrayList<>();
-
-        if (words.length == 0 || s.length() == 0) {
-            return ans;
+        List<Integer> ll = new ArrayList<>();
+        if (s.length() == 0 || words.length == 0) {
+            return ll;
         }
 
-        int wordSize = words[0].length();
-        int wordCount = words.length;
-        int N = s.length();
+        int wordLen = words[0].length();
+        int wordC = words.length;
+        int totalLen = wordLen * wordC;
 
-        HashMap<String,Integer> originalCount = new HashMap<>();
-        for(int i = 0; i<words.length; i++){
-            originalCount.put(words[i], originalCount.getOrDefault(words[i],0)+1);
+        HashMap<String, Integer> originalC = new HashMap<>();
+        for (String str : words) {
+            originalC.put(str, originalC.getOrDefault(str, 0) + 1);
         }
 
-        for(int offset = 0; offset<wordSize; offset++){
-            HashMap<String,Integer> currentCount = new HashMap<>();
+        for (int offset = 0; offset < wordLen; offset++) {
+            HashMap<String, Integer> currC = new HashMap<>();
             int start = offset;
             int count = 0;
-            for(int end = offset; end + wordSize <= N; end += wordSize){
-                String currWord = s.substring(end, end + wordSize);
-                if(originalCount.containsKey(currWord)){
-                    currentCount.put(currWord, currentCount.getOrDefault(currWord,0)+1);
+
+            for (int end = offset; end + wordLen <= s.length(); end += wordLen) {
+                String currS = s.substring(end, end + wordLen); 
+
+                if (originalC.containsKey(currS)) {
+                    currC.put(currS, currC.getOrDefault(currS, 0) + 1);
                     count++;
 
-                    while(currentCount.get(currWord)>originalCount.get(currWord)){
-                        String startWord = s.substring(start,start+wordSize);
-                        currentCount.put(startWord, currentCount.get(startWord)-1);
-                        start+=wordSize;
-                        count--;                        
+                    while (currC.get(currS) > originalC.get(currS)) {
+                        String startWord = s.substring(start, start + wordLen);
+                        currC.put(startWord, currC.get(startWord) - 1);
+                        start += wordLen;
+                        count--;
                     }
 
-                    if(count == wordCount){
-                        ans.add(start);
+                    if (count == wordC) {
+                        ll.add(start);
                     }
-                    
-                }
-                else{
+
+                } else {
+                    currC.clear();
                     count = 0;
-                    start = end + wordSize;
-                    currentCount.clear();
+                    start = end + wordLen;
                 }
             }
-
         }
-        return ans;
+
+        return ll;
     }
 }
