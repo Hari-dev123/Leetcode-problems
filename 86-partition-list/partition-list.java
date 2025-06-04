6ede12c096c1241b6c1723d8e8140fbbad4c7eed
HARIPRASATH G <140241512+Hari-dev123@users.noreply.github.com>
2025-06-04 13:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (29.97%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -1,23 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode beforeHead = new ListNode(0); // Dummy head for < x
-        ListNode afterHead = new ListNode(0);  // Dummy head for >= x
-        ListNode before = beforeHead;
-        ListNode after = afterHead;
-        
-        while (head != null) {
-            if (head.val < x) {
-                before.next = head;
-                before = before.next;
-            } else {
-                after.next = head;
-                after = after.next;
-            }
-            head = head.next;
-        }
-        
-        after.next = null;          // Important to avoid cycle
-        before.next = afterHead.next;  // Join both partitions
-        return beforeHead.next;
+          
+          ListNode part1 = new ListNode(0);
+          ListNode temp1 = part1;
+          ListNode part2 = new ListNode(0);
+          ListNode temp2 = part2;
+          while(head != null ){
+              if(head.val < x){
+                 temp1.next = new ListNode(head.val);
+                 temp1 = temp1.next;
+              }else{
+                  temp2.next = new ListNode(head.val);
+                 temp2 = temp2.next;
+              }
+              head = head.next;
+          }
+          temp2.next =null;
+          temp1.next = part2.next;
+          return part1.next;
     }
 }
